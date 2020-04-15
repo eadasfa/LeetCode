3da5a89a31b68edc505e44f640b678c91d9b58cc
dq <1319468035@qq.com>
2020-04-15 16:30:54 +0800

    添加了题目详情

--- 16.最接近的三数之和.py
+++ 16.最接近的三数之和.py
@@ -30,3 +30,10 @@ class Solution(object):
 # print(Solution().threeSumClosest([-1,2,1,-4],1))
 # @lc code=end
 
+'''
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+
+例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+
+与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+'''
